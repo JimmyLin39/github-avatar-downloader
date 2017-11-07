9681a8c6f3f6c7a6ca0ef6f9def1bc57002cedbc
JimmyLin39 <jimmylin0309@gmail.com>
2017-11-07 02:57:33 +0000

    clean up and connect the functions

--- download_avatars.js
+++ download_avatars.js
@@ -16,50 +16,46 @@ function getRepoContributors(repoOwner, repoName, cb) {
   request(options, function(err, res, body) {
     // change response body from JSON to JS
     let bodyObj = JSON.parse(body);
-
-    // the bodyObj is an array contain objects
-    // for each item in bodyObj find the object key avatar_url
-    bodyObj.forEach(function(item){
-      //console.log('avatar_url: ', item.avatar_url);
-      //console.log(item);
-
-      let avatarURL = item.avatar_url;
-      let username = item.login;
-      //cb(avatarURL,username) ;
-      downloadImageByURL(avatarURL, username);
-    });
-
-    // cb(err, avatarURL);
+    // call back function find the avatar_url and username
+    cb(err, bodyObj);
   });
 }
 
+//
 function downloadImageByURL(url, filePath) {
   //console.log('url', url)
-  request.get(url)
-         .on('error', function(err){
-          console.log('err');
-          throw err;
-       })
-         .on('response', function(response){
-          console.log('download...')
-         })
-         .on('end', function(){
-          console.log('Download complete.');
-         })
-         .pipe(fs.createWriteStream('./avatars/'+filePath+'.jpg'));
-
+  request
+    .get(url)
+    .on('error', function(err) {
+      console.log('err');
+      throw err;
+    })
+    // .on('response', function(response) {
+    //   console.log('download...')
+    // })
+    .on('end', function() {
+      console.log('Download complete.');
+    })
+    .pipe(fs.createWriteStream('./avatars/'+filePath+'.jpg'));
 }
 
 var repoOwner = process.argv[2];
 var repoName = process.argv[3];
 
-if(!repoOwner){
+if (!repoOwner) {
   console.log('please enter a repo owner.')
-}else if (!repoName) {
+} else if (!repoName) {
   console.log('please enter a repo name.')
-}else{
-  getRepoContributors(repoOwner, repoName, function(url, username) {
-  console.log("url:", url);
-  console.log("username:", username);
+} else {
+  getRepoContributors(repoOwner, repoName, function(err, data) {
+
+    // the bodyObj is an array contain objects
+    // for each item in bodyObj find the object key avatar_url
+    data.forEach(function(item){
+      let avatarURL = item.avatar_url;
+      let username = item.login;
+      downloadImageByURL(avatarURL, username);
+    });
   });
+
 }
