7f6fcb87bb25e87e51e0d4d072540f08b2bb2fcb
JimmyLin39 <jimmylin0309@gmail.com>
2017-11-07 01:52:58 +0000

    Implement downloadImageByURL

--- download_avatars.js
+++ download_avatars.js
@@ -1,6 +1,6 @@
 var request = require('request');
 var secrets = require('./secrets.js');
-console.log(secrets.GITHUB_TOKEN);
+var fs = require('fs');
 
 console.log('Welcome to the GitHub Avatar Downloader!');
 
@@ -16,15 +16,31 @@ function getRepoContributors(repoOwner, repoName, cb) {
   request(options, function(err, res, body) {
     // change response body from JSON to JS
     let bodyObj = JSON.parse(body);
+
     // the bodyObj is an array contain objects
     // for each item in bodyObj find the object key avatar_url
     bodyObj.forEach(function(item){
-      console.log('avatar_url: ', item.avatar_url);
+      //console.log('avatar_url: ', item.avatar_url);
+      //console.log(item);
+
+      let avatarURL = item.avatar_url;
+      let username = item.login;
+      downloadImageByURL(avatarURL, username);
     });
-
     //cb(err, body);
-
+    console.log('Download complete.');
   });
+}
+
+function downloadImageByURL(url, filePath) {
+  console.log('url', url)
+  request.get(url)
+         .on('error', function(err){
+          console.log('err');
+          throw err;
+       })
+         .pipe(fs.createWriteStream('./avatars/'+filePath+'.jpg'));
+
 }
 
 getRepoContributors("jquery", "jquery", function(err, result) {
